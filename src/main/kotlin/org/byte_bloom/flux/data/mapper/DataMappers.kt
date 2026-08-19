3bb5a3b9f74d026fcb66ebbd971f8fd6004dade2
sara alajjouri <saraalajjouri@gmail.com>
2026-08-20 00:41:58 +0300

    refactor: clean up DataMappers and fix detekt issues

--- src/main/kotlin/org/byte_bloom/flux/data/mapper/DataMappers.kt
+++ src/main/kotlin/org/byte_bloom/flux/data/mapper/DataMappers.kt
@@ -1,9 +1,9 @@
 package org.byte_bloom.flux.data.mapper
 
-import org.byte_bloom.flux.data.dataholders.PackageRaw
 import org.byte_bloom.flux.data.dataholders.Priority
-import org.byte_bloom.flux.data.dataholders.RouteRaw
+import org.byte_bloom.flux.data.dataholders.PackageRaw
 import org.byte_bloom.flux.data.dataholders.VehicleRaw
+import org.byte_bloom.flux.data.dataholders.RouteRaw
 import org.byte_bloom.flux.data.dataholders.WarehouseRaw
 import org.byte_bloom.flux.domain.model.Package
 import org.byte_bloom.flux.domain.model.Route
@@ -11,47 +11,34 @@ import org.byte_bloom.flux.domain.model.Vehicle
 import org.byte_bloom.flux.domain.model.Warehouse
 import org.byte_bloom.flux.domain.model.Priority as DomainPriority
 
+private const val DEFAULT_COORDINATE = 0.0
+
+private fun createEmptyWarehouse(id: String) = Warehouse(
+    id = id, name = "", regionalZone = "",
+    latitude = DEFAULT_COORDINATE, longitude = DEFAULT_COORDINATE
+)
+
 fun WarehouseRaw.toDomain() = Warehouse(
-    id = id,
-    name = name,
-    regionalZone = regionalZone,
-    latitude = latitude,
-    longitude = longitude
+    id = id, name = name, regionalZone = regionalZone,
+    latitude = latitude, longitude = longitude
 )
 
 fun VehicleRaw.toDomain() = Vehicle(
-    id = id,
-    maxCapacityKg = maxCapacityKg,
-    costPerKm = costPerKm,
-    currentHub = Warehouse(
-        id = currentHubId,
-        name = "",
-        regionalZone = "",
-        latitude = 0.0,
-        longitude = 0.0
-    )
+    id = id, maxCapacityKg = maxCapacityKg, costPerKm = costPerKm,
+    currentHub = createEmptyWarehouse(currentHubId)
 )
 
-fun PackageRaw.toDomain(
-    origin: Warehouse,
-    destination: Warehouse
-) = Package(
-    id = id,
-    weight = weight,
-    originHub = origin,
-    destinationHub = destination,
+fun PackageRaw.toDomain() = Package(
+    id = id, weight = weight,
+    originHub = createEmptyWarehouse(originHubId),
+    destinationHub = createEmptyWarehouse(destinationHubId),
     priority = priority.toDomain()
 )
 
-fun RouteRaw.toDomain(
-    origin: Warehouse,
-    destination: Warehouse
-) = Route(
-    id = id,
-    distanceKm = distanceKm,
-    typicalDelayMin = typicalDelayMin,
-    originHub = origin,
-    destinationHub = destination
+fun RouteRaw.toDomain() = Route(
+    id = id, distanceKm = distanceKm, typicalDelayMin = typicalDelayMin,
+    originHub = createEmptyWarehouse(originHubId),
+    destinationHub = createEmptyWarehouse(destinationHubId)
 )
 
 fun Priority.toDomain(): DomainPriority = when (this) {
