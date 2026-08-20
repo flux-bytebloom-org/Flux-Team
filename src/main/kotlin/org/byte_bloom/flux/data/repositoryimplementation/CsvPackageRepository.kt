b240b968c3f3b32d6c9a59493800e8d8de108e38
sara alajjouri <saraalajjouri@gmail.com>
2026-08-20 14:57:55 +0300

    fix: detekt issues

--- src/main/kotlin/org/byte_bloom/flux/data/repositoryimplementation/CsvPackageRepository.kt
+++ src/main/kotlin/org/byte_bloom/flux/data/repositoryimplementation/CsvPackageRepository.kt
@@ -14,6 +14,7 @@ class CsvPackageRepository(
     override fun getAll(): List<Package> {
         val lines = readCsv(filePath)
         val cleanedLines = cleanLines(lines)
-        return parsePackages(cleanedLines).map { it.toDomain() }
+        return parsePackages(cleanedLines).map {
+            it.toDomain()}
     }
 }
