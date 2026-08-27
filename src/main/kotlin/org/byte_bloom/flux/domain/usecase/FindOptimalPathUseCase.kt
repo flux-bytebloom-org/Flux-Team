9c562e00b4a6c01896781a4be646636e1154a70c
Nada Salama <nhussam827@gmail.com>
2026-08-27 13:16:45 +0300

    feat: add FindOptimalPathUseCase

---
+++ src/main/kotlin/org/byte_bloom/flux/domain/usecase/FindOptimalPathUseCase.kt
@@ -0,0 +1,12 @@
+package org.byte_bloom.flux.domain.usecase
+
+import org.byte_bloom.flux.domain.logic.routing.DijkstraRouter
+import org.byte_bloom.flux.domain.model.Warehouse
+
+class FindOptimalPathUseCase (private val dijkstraRouter: DijkstraRouter){
+    operator fun invoke(start: Warehouse, destination: Warehouse): List<Warehouse> {
+
+        return dijkstraRouter.findShortestPath(start, destination)
+    }
+
+}
