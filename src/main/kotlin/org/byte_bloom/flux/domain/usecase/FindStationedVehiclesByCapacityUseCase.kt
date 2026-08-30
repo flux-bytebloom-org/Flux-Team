a028348f1f0cbb23488367250b7239b5678d67e3
Nada Salama <nhussam827@gmail.com>
2026-08-30 14:21:20 +0300

    feat: add invalid required weight exception

--- src/main/kotlin/org/byte_bloom/flux/domain/usecase/FindStationedVehiclesByCapacityUseCase.kt
+++ src/main/kotlin/org/byte_bloom/flux/domain/usecase/FindStationedVehiclesByCapacityUseCase.kt
@@ -1,16 +1,18 @@
 package org.byte_bloom.flux.domain.usecase
 
+import org.byte_bloom.flux.domain.exception.InvalidRequiredWeightException
 import org.byte_bloom.flux.domain.model.Vehicle
 import org.byte_bloom.flux.domain.model.Warehouse
 
 class FindStationedVehiclesByCapacityUseCase {
     operator fun invoke(warehouse: Warehouse, requiredWeight: Double): List<Vehicle> {
 
-        require(requiredWeight >= 0) {
-            "Required weight cannot be negative"
+        if (requiredWeight < 0) {
+            throw InvalidRequiredWeightException(
+                "Required weight cannot be negative"
+            )
         }
-
         return warehouse.getStationedVehicles()
             .filter { it.maxCapacityKg >= requiredWeight }
-    }
+        }
 }
