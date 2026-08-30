ec4325dd79351d39d62577f8f6b2a302a33291b1
Nada Salama <nhussam827@gmail.com>
2026-08-30 16:49:47 +0300

    feat: add lowest cost vehicle assignment use case

---
+++ src/main/kotlin/org/byte_bloom/flux/domain/usecase/AssignPackageToLowestCostStationedVehicleUseCase.kt
@@ -0,0 +1,34 @@
+package org.byte_bloom.flux.domain.usecase
+
+import org.byte_bloom.flux.domain.exception.InvalidPackageWeightException
+import org.byte_bloom.flux.domain.model.Package
+import org.byte_bloom.flux.domain.model.Vehicle
+import org.byte_bloom.flux.domain.model.Warehouse
+
+class AssignPackageToLowestCostStationedVehicleUseCase(
+    private val findStationedVehiclesByCapacityUseCase:
+    FindStationedVehiclesByCapacityUseCase
+) {
+
+    operator fun invoke(
+        warehouse: Warehouse,
+        packageItem: Package,
+        distanceKm: Double
+    ): Vehicle? {
+
+        val requiredWeight = packageItem.weight
+            ?: throw InvalidPackageWeightException(
+                "Package weight cannot be null"
+            )
+
+        val eligibleVehicles =
+            findStationedVehiclesByCapacityUseCase(
+                warehouse,
+                requiredWeight
+            )
+
+        return eligibleVehicles.minByOrNull { vehicle ->
+            distanceKm * vehicle.costPerKm
+        }
+    }
+}
