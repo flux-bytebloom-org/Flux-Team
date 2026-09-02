b0f5032dd354b032c2e5494291268ecc251359c1
sara alajjouri <saraalajjouri@gmail.com>
2026-09-02 13:58:10 +0300

    Fix magic number violations flagged by detekt

--- src/main/kotlin/org/byte_bloom/flux/domain/usecase/AnalyzeTreePerformanceUseCase.kt
+++ src/main/kotlin/org/byte_bloom/flux/domain/usecase/AnalyzeTreePerformanceUseCase.kt
@@ -3,11 +3,16 @@ package org.byte_bloom.flux.domain.usecase
 import org.byte_bloom.flux.domain.logic.tree.BinarySearchTree
 import org.byte_bloom.flux.domain.logic.tree.buildBalancedInsertionOrder
 
+private const val TRACKING_ID_RANGE_START = 1
+private const val TRACKING_ID_COUNT = 1000
+private const val TRACKING_ID_NUMBER_WIDTH = 6
+private const val TRACKING_ID_PADDING_CHAR = '0'
+
 class AnalyzeTreePerformanceUseCase {
     operator fun invoke() {
         val trackingIds = mutableListOf<String>()
-        for (number in 1..1000) {
-            trackingIds.add("PKG-" + number.toString().padStart(6, '0'))
+        for (number in TRACKING_ID_RANGE_START..TRACKING_ID_COUNT) {
+            trackingIds.add("PKG-" + number.toString().padStart(TRACKING_ID_NUMBER_WIDTH, TRACKING_ID_PADDING_CHAR))
         }
 
         val unbalancedTree = BinarySearchTree()
