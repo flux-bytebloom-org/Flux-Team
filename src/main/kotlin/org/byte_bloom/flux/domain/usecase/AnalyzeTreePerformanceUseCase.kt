99db05c59edadd09e3db1615ad0e6afa37611a33
abdelrahman Shat <abdelrahman.hayel@gmail.com>
2026-09-04 18:36:34 +0300

    refactor: generate tracking IDs with map and insert via forEach
    Fixes Sub-Task 4.3 and keeps domain.usecase free of for/while loops
    (Sub-Task 2.1/2.2 mandatory criteria).

--- src/main/kotlin/org/byte_bloom/flux/domain/usecase/AnalyzeTreePerformanceUseCase.kt
+++ src/main/kotlin/org/byte_bloom/flux/domain/usecase/AnalyzeTreePerformanceUseCase.kt
@@ -10,24 +10,19 @@ private const val TRACKING_ID_PADDING_CHAR = '0'
 
 class AnalyzeTreePerformanceUseCase {
     operator fun invoke() {
-        val trackingIds = mutableListOf<String>()
-        for (number in TRACKING_ID_RANGE_START..TRACKING_ID_COUNT) {
-            trackingIds.add("PKG-" + number.toString().padStart(TRACKING_ID_NUMBER_WIDTH, TRACKING_ID_PADDING_CHAR))
-        }
+        val trackingIds = (TRACKING_ID_RANGE_START..TRACKING_ID_COUNT)
+            .map { number ->
+                "PKG-" + number.toString().padStart(TRACKING_ID_NUMBER_WIDTH, TRACKING_ID_PADDING_CHAR)
+            }
 
         val unbalancedTree = BinarySearchTree()
-        for (id in trackingIds) {
-            unbalancedTree.insert(id)
-        }
+        trackingIds.forEach(unbalancedTree::insert)
 
         val balancedInsertionOrder = buildBalancedInsertionOrder(trackingIds)
         val balancedTree = BinarySearchTree()
-        for (id in balancedInsertionOrder) {
-            balancedTree.insert(id)
-        }
+        balancedInsertionOrder.forEach(balancedTree::insert)
 
         val targetId = trackingIds.last()
-
         val unbalancedSteps = unbalancedTree.searchStepCount(targetId)
         val balancedSteps = balancedTree.searchStepCount(targetId)
 
