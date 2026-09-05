c14a51c1e78313b549fedbffa7cde31c6d6af276
abdelrahman Shat <abdelrahman.hayel@gmail.com>
2026-09-05 13:01:54 +0300

    feat: improve rerouting logic in package and shipment use cases for better accuracy and less exceptions

--- src/main/kotlin/org/byte_bloom/flux/domain/usecase/ReroutePackageUseCase.kt
+++ src/main/kotlin/org/byte_bloom/flux/domain/usecase/ReroutePackageUseCase.kt
@@ -3,6 +3,7 @@ package org.byte_bloom.flux.domain.usecase
 import org.byte_bloom.flux.domain.exception.UseCaseException
 import org.byte_bloom.flux.domain.model.Package
 import org.byte_bloom.flux.domain.model.Warehouse
+import org.byte_bloom.flux.ui.utils.logWarning
 
 class ReroutePackageUseCase {
     operator fun invoke(
@@ -10,16 +11,13 @@ class ReroutePackageUseCase {
         packageItem: Package,
         newDestination: Warehouse
     ): Package {
-
-        if (packageItem !in originHub.getCargoQueue()) {
-            throw UseCaseException.PackageNotInQueue(
+        val actualPackage = originHub.getCargoQueue().find { it.id == packageItem.id }
+            ?: throw UseCaseException.PackageNotInQueue(
                 packageId = packageItem.id,
                 warehouseId = originHub.id
             )
-        }
-        val reroutedPackage = packageItem.copy(destinationHub = newDestination)
-
-        originHub.removePackage(packageItem)
+        val reroutedPackage = actualPackage.copy(destinationHub = newDestination)
+        originHub.removePackage(actualPackage)
         originHub.addPackage(reroutedPackage)
 
         return reroutedPackage
