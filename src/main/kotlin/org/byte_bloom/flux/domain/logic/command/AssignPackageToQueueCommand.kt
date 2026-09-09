ec0f412c709a3cbfcf40017696d73b0835d9095f
abdelrahman Shat <abdelrahman.hayel@gmail.com>
2026-09-09 10:30:58 +0300

    feat(command): refactor AssignPackageToQueueCommand to use AssignPackageToCargoQueueUseCase for package assignment

--- src/main/kotlin/org/byte_bloom/flux/domain/logic/command/AssignPackageToQueueCommand.kt
+++ src/main/kotlin/org/byte_bloom/flux/domain/logic/command/AssignPackageToQueueCommand.kt
@@ -2,14 +2,16 @@ package org.byte_bloom.flux.domain.logic.command
 
 import org.byte_bloom.flux.domain.model.Package
 import org.byte_bloom.flux.domain.model.Warehouse
+import org.byte_bloom.flux.domain.usecase.AssignPackageToCargoQueueUseCase
 
 class AssignPackageToQueueCommand(
     private val hub: Warehouse,
-    private val packageItem: Package
+    private val packageItem: Package,
+    private val assignPackageToCargoQueueUseCase: AssignPackageToCargoQueueUseCase
 ) : Command {
 
     override fun execute() {
-        hub.addPackage(packageItem)
+        assignPackageToCargoQueueUseCase(hub, packageItem)
     }
 
     override fun undo() {
