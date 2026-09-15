e47272643495e636a9a2bdefd3dd24698b030fdd
sameera sweedan <sn4210729272004@gmail.com>
2026-09-15 15:33:30 +0300

    Fix PackageRequestDto field naming and add missing newline in VehicleRequestDto

--- src/main/kotlin/org/byte_bloom/flux/data/remote/dto/PackageRequestDto.kt
+++ src/main/kotlin/org/byte_bloom/flux/data/remote/dto/PackageRequestDto.kt
@@ -1,13 +1,14 @@
 package org.byte_bloom.flux.data.remote.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-
 
 @Serializable
 data class PackageRequestDto(
     val weight: Double?,
-    val origin_hub_id: String,
-    val destination_hub_id: String,
+    @SerialName("origin_hub_id")
+    val originHubId: String,
+    @SerialName("destination_hub_id")
+    val destinationHubId: String,
     val priority: String
 )
-
