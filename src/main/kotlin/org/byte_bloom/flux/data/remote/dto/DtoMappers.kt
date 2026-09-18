51ff15b433aa723ca6e6bda6a9c42440ec8b95d8
abdelrahman Shat <abdelrahman.hayel@gmail.com>
2026-09-18 21:31:40 +0300

    feat(mapping): update VehicleResponseDto.toDomain to include warehouses mapping

--- src/main/kotlin/org/byte_bloom/flux/data/remote/dto/DtoMappers.kt
+++ src/main/kotlin/org/byte_bloom/flux/data/remote/dto/DtoMappers.kt
@@ -68,12 +68,18 @@ fun Package.toRequestDto(): PackageRequestDto {
 
 // ---------- Vehicle ----------
 
-fun VehicleResponseDto.toDomain(): Vehicle {
+fun VehicleResponseDto.toDomain(warehousesById: Map<String, Warehouse>): Vehicle {
+    val vehiclesHub = warehousesById[currentHubId]
+    if (vehiclesHub == null ) return null
+
+    // val hub = warehousesById[currentHubId]
+    //        ?: throw DomainException.ResourceNotFoundException(currentHubId)
+
     return Vehicle(
         id = id,
         maxCapacityKg = maxCapacityKg,
         costPerKm = costPerKm,
-        currentHub = createEmptyWarehouse(currentHubId)
+        currentHub = vehiclesHub
     )
 }
 
