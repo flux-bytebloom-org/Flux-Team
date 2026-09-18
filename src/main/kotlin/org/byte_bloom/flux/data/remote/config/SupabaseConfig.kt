2cc4be060939e7f541d940f052441b226fd5b0dd
Nada Salama <nhussam827@gmail.com>
2026-09-18 15:35:27 +0300

    fix: add missing /rest/v1 PostgREST path suffix to SupabaseConfig.BASE_URL

--- src/main/kotlin/org/byte_bloom/flux/data/remote/config/SupabaseConfig.kt
+++ src/main/kotlin/org/byte_bloom/flux/data/remote/config/SupabaseConfig.kt
@@ -1,6 +1,6 @@
 package org.byte_bloom.flux.data.remote.config
 
 object SupabaseConfig {
-    const val BASE_URL = "https://uihrwuwrwvbzfcssxrkc.supabase.co"
+    const val BASE_URL = "https://uihrwuwrwvbzfcssxrkc.supabase.co/rest/v1"
     const val API_KEY = "sb_publishable_JW_FRytXkKDR-VjEk0qtsA_Qm0TMi3X"
 }
