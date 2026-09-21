f59fd65040e419562a56f5c6f94da61b045edb1d
Nada Salama <nhussam827@gmail.com>
2026-09-21 11:06:24 +0300

    feat(usecase): validate warehouse creation

--- src/main/kotlin/org/byte_bloom/flux/domain/usecase/crud/CreateWarehouseUseCase.kt
+++ src/main/kotlin/org/byte_bloom/flux/domain/usecase/crud/CreateWarehouseUseCase.kt
@@ -2,7 +2,23 @@ package org.byte_bloom.flux.domain.usecase.crud
 
 import org.byte_bloom.flux.domain.model.Warehouse
 import org.byte_bloom.flux.domain.repository.WarehouseRepository
+import org.byte_bloom.flux.domain.validator.ValidationResult
+import org.byte_bloom.flux.domain.validator.warehouseValidations.WarehouseCreateValidator
 
-class CreateWarehouseUseCase(private val repository: WarehouseRepository) {
-    suspend operator fun invoke(warehouse: Warehouse): Warehouse = repository.create(warehouse)
+class CreateWarehouseUseCase(
+    private val repository: WarehouseRepository,
+    private val validator: WarehouseCreateValidator
+) {
+    suspend operator fun invoke(warehouse: Warehouse): Warehouse {
+
+        val validation = validator(warehouse)
+
+        if (validation is ValidationResult.Invalid) {
+            throw IllegalArgumentException(
+                validation.errors.joinToString(", ")
+            )
+        }
+
+        return repository.create(warehouse)
+    }
 }
