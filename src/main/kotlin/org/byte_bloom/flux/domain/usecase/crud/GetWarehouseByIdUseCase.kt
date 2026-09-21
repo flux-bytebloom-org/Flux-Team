0b551127b96655a981c78c8e48779eccc816c28e
Nada Salama <nhussam827@gmail.com>
2026-09-21 11:42:32 +0300

    feat(usecase): validate warehouse id on retrieval

--- src/main/kotlin/org/byte_bloom/flux/domain/usecase/crud/GetWarehouseByIdUseCase.kt
+++ src/main/kotlin/org/byte_bloom/flux/domain/usecase/crud/GetWarehouseByIdUseCase.kt
@@ -2,7 +2,24 @@ package org.byte_bloom.flux.domain.usecase.crud
 
 import org.byte_bloom.flux.domain.model.Warehouse
 import org.byte_bloom.flux.domain.repository.WarehouseRepository
+import org.byte_bloom.flux.domain.validator.EntityPrefixes
+import org.byte_bloom.flux.domain.validator.IdValidator
+import org.byte_bloom.flux.domain.validator.ValidationResult
 
-class GetWarehouseByIdUseCase(private val repository: WarehouseRepository) {
-    suspend operator fun invoke(id: String): Warehouse? = repository.getById(id)
+class GetWarehouseByIdUseCase(
+    private val repository: WarehouseRepository,
+    private val idValidator: IdValidator = IdValidator(EntityPrefixes.WAREHOUSE)
+) {
+    suspend operator fun invoke(id: String): Warehouse? {
+
+        val validation = idValidator(id)
+
+        if (validation is ValidationResult.Invalid) {
+            throw IllegalArgumentException(
+                validation.errors.joinToString(", ")
+            )
+        }
+
+        return repository.getById(id)
+    }
 }
