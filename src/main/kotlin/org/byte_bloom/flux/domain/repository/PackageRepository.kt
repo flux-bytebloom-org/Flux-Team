a97ac5704c5c1427690a80b6e479930fcaf6f5d3
Nada Salama <nhussam827@gmail.com>
2026-09-23 02:04:25 +0300

    refactor: change PackageRepository interface to return Result<T>

--- src/main/kotlin/org/byte_bloom/flux/domain/repository/PackageRepository.kt
+++ src/main/kotlin/org/byte_bloom/flux/domain/repository/PackageRepository.kt
@@ -6,18 +6,16 @@ import org.byte_bloom.flux.domain.model.Warehouse
 interface PackageRepository {
     suspend fun getAll(): List<Package>
 
-    suspend fun getById(id: String): Package
+    suspend fun getById(id: String): Result<Package>
 
-    suspend fun create(pkg: Package): Package
+    suspend fun create(pkg: Package): Result<Package>
 
-    suspend fun update(id: String, pkg: Package): Package
+    suspend fun update(id: String, pkg: Package): Result<Package>
 
-    suspend fun delete(id: String)
+    suspend fun delete(id: String): Result<Unit>
 
-    /** Updates the package's origin hub AND places it in that hub's cargo sorted queue. */
     fun updatePackageOriginHub(pkg: Package, hub: Warehouse): Package
 
-    /** Removes the package's hub association AND removes it from that hub's cargo queue*/
     fun removePackageFromHub(pkg: Package, hub: Warehouse)
 
     fun updatePackageDestination(pkg: Package, newDestination: Warehouse): Package
